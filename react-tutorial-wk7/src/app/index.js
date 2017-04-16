08eb9439a4d9be4ec7dc36f2388a80ba2757e571
Patrick Logan <JPatrickLogan@gmail.com>
2017-04-15 23:54:26 -0600

    feat(event)User Input function added

--- react-tutorial-wk7/src/app/index.js
+++ react-tutorial-wk7/src/app/index.js
@@ -6,10 +6,6 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
   render() {
-      let user = {
-        name: "Anna",
-        hobbies: ["Sports", "Reading"]
-      };
     return (
       <div className = "container">
         <div className = "row">
@@ -19,8 +15,7 @@ class App extends React.Component {
         </div>
         <div className = "row">
           <div className = "col-xs-10 col-xs-offset-1">
-            <Home name={"Maximillian"} age={27} user = {user}/>
-              <p>This is a paragraph</p>
+            <Home name={"Maximillian"} age={27}/>
           </div>
         </div>
       </div>
